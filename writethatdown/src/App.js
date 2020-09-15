c4c99fc5c5b1dafedaf613936fa46667311943f9
jarydf <jaryd_fisher@hotmail.com>
2020-09-15 13:21:27 -0700

    first production commit

--- writethatdown/src/App.js
+++ writethatdown/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p> hello world </p>
+        <p> hello world my name is jaryd</p>
       </header>
     </div>
   );
